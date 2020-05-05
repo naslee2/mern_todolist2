5bcee2374604e097ad36e178d30fded4ac0d2776
Nathan <naslee2@gmail.com>
2020-05-04 23:12:31 -0700

    update to server code

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,30 @@ app.use(cors());
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost/user', { useNewUrlParser: true });
+// mongoose.Promise = global.Promise; //for version 4 mongodb
+
 const connection = mongoose.connection;
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+let Todo = new Schema({
+    todo_description: {
+        type: String
+    },
+    todo_responsible: {
+        type: String
+    },
+    todo_priority: {
+        type: String
+    },
+    todo_completed: {
+        type: Boolean
+    }
+});
+
+module.exports = mongoose.model('Todo', Todo);
+
 
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
