13af2c30fc1834cb2187463e8713f77f1d93636a
Nathan <naslee2@gmail.com>
2020-05-13 21:44:01 -0700

    update

--- mern-todo/src/Components/edit_todo.js
+++ mern-todo/src/Components/edit_todo.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
 
+const Todo = props => (
+    <tr>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_description}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_responsible}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_priority}</td>
+        <td>
+            <Link to={"/edit/"+props.todo._id}>Edit</Link>
+        </td>
+    </tr>
+)
+
 export default class EditTodo extends Component {
     render() {
         return (
